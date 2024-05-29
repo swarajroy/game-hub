159b22a7d17ba460012aa8fcf1a6d9ef5f8a04d0
Swaraj Roy <contactswarajroy@gmail.com>
2024-05-29 21:39:21 +0100

    added api key in env

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
-  params: { key: "7dd51e9f46324536b04538f1466ad7aa" },
+  params: { key: import.meta.env.VITE_RAWG_API_KEY },
 });
