c37258bd5ac0c2b269eee2fa6bd243dc186e264f
Swaraj Roy <contactswarajroy@gmail.com>
2024-09-06 18:45:21 +0100

    added ms to simplify time calculations

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
-import APIClient from "../services/api-client";
-import genres from "../data/genres";
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
+import genres from "../data/genres";
+import APIClient from "../services/api-client";
 
 const apiClient = new APIClient<Genre>("/genres");
 export interface Genre {
@@ -13,7 +14,7 @@ const useGenres = () =>
   useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 24 * 60 * 60 * 1000, // 24 hrs
+    staleTime: ms("24h"), //24 hrs
     initialData: genres,
   });
 
